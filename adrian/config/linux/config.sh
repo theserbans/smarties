7c083f35bfc087a0cb7385b19a78aad59ee3fe75
Adrian Serban <adrian.serban@sympatico.ca>
2015-09-19 12:52:38 -0400

    multiple changes

--- adrian/config/linux/config.sh
+++ adrian/config/linux/config.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# bash
+mv ~/.bashrc ~/.bashrc.bkp
+cp .bashrc ~/.bashrc
+mv ~/.profile ~/.profile.bkp
+cp .profile ~/.profile
+
+
 # vim
 cp .vimrc ~/.vimrc
 
@@ -11,7 +18,13 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/plasticboy/vim-markdown.git
 git clone https://github.com/nelstrom/vim-markdown-folding.git
 git clone https://github.com/will133/vim-dirdiff.git
+
+
+## vifm  TODO
+ln -s ./config/vifm/vifmrc .vifmrc
 
