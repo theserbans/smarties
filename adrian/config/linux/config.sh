eb7d547c3fc2f16f3993460e786a880e778a3f94
U-NEXJSYSTEMS\adrian.serban <adrian.serban@tor-lt-aserban.nexjsystems.local>
2016-03-01 17:33:24 -0500

    updates

--- adrian/config/linux/config.sh
+++ adrian/config/linux/config.sh
@@ -5,10 +5,12 @@ mv ~/.bashrc ~/.bashrc.bkp
 cp .bashrc ~/.bashrc
 mv ~/.profile ~/.profile.bkp
 cp .profile ~/.profile
+cp .bash_profile ~/.bash_profile
 
 
 # vim
 cp .vimrc ~/.vimrc
+cp .config ~/.config
 
 mkdir ~/.vim
 cp .vim/colors ~/.vim/colors
@@ -23,8 +25,4 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/plasticboy/vim-markdown.git
 git clone https://github.com/nelstrom/vim-markdown-folding.git
 git clone https://github.com/will133/vim-dirdiff.git
-
-
-## vifm  TODO
-ln -s ./config/vifm/vifmrc .vifmrc
 
--- adrian/config/linux/get-sources.sh
+++ adrian/config/linux/get-sources.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/vifm/vifm.git
-# git clone https://github.com/tmux/tmux.git
-git clone https://github.com/thewtex/tmux-mem-cpu-load
-git clone https://github.com/hishamhm/htop
+git clone 'https://github.com/vifm/vifm.git'
+# git clone 'https://github.com/tmux/tmux.git'
+git clone 'https://github.com/thewtex/tmux-mem-cpu-load'
+git clone 'https://github.com/hishamhm/htop'
+git clone 'https://github.com/flok99/multitail'
